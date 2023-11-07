9f781ba10aa8773f6f0965a001b91dff5f392617
Schuyler Conover <schuyler.conover@qgenda.com>
2023-11-07 13:21:47 -0500

    sample project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using GdPicture14;
+using GdPicture;
+
+LicenseManager licenseManager = new LicenseManager();
+licenseManager.RegisterKEY("XXXXXXXX");
+
+using (var outputPdfStream = new MemoryStream())
+using (var basePdfStream = new MemoryStream(File.ReadAllBytes("Document.pdf")))
+using (var jsonStream = new MemoryStream(File.ReadAllBytes("json.json")))
+using (var fileStream = new FileStream("DocumentWithJson.pdf", FileMode.Create, FileAccess.Write))
+using (var basePdf = new GdPicturePDF())
+{
+    GdPictureStatus result = basePdf.LoadFromStream(basePdfStream);
+
+    result = basePdf.ImportInstantJSONDataFromStream(jsonStream);
+
+    result = basePdf.SaveToStream(outputPdfStream);
+
+    outputPdfStream.CopyTo(fileStream);
+}
