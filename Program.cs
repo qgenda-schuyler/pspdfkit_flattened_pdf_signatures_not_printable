3c309f0168ee40daaf946ad01bbbb06f8fb1ddbb
Schuyler Conover <schuyler.conover@qgenda.com>
2024-05-15 12:00:55 -0400

    PSPDFKit signatures not showing up on print problem

--- Program.cs
+++ Program.cs
@@ -2,19 +2,18 @@
 using GdPicture;
 
 LicenseManager licenseManager = new LicenseManager();
-licenseManager.RegisterKEY("XXXXXXXX");
+licenseManager.RegisterKEY("");
 
-using (var outputPdfStream = new MemoryStream())
 using (var basePdfStream = new MemoryStream(File.ReadAllBytes("Document.pdf")))
 using (var jsonStream = new MemoryStream(File.ReadAllBytes("json.json")))
-using (var fileStream = new FileStream("DocumentWithJson.pdf", FileMode.Create, FileAccess.Write))
+using (var outputPdfStream = new FileStream("FlattenedDocument.pdf", FileMode.Create, FileAccess.Write))
 using (var basePdf = new GdPicturePDF())
 {
     GdPictureStatus result = basePdf.LoadFromStream(basePdfStream);
 
     result = basePdf.ImportInstantJSONDataFromStream(jsonStream);
 
-    result = basePdf.SaveToStream(outputPdfStream);
+    result = basePdf.FlattenFormFields();
 
-    outputPdfStream.CopyTo(fileStream);
+    result = basePdf.SaveToStream(outputPdfStream);
 }
